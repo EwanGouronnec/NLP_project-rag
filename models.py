435cc1708e71e8bd7f30327359e090ed17ff83ed
Ewan <gouronnece@cy-tech.Fr>
2025-01-11 12:07:23 +0100

    embedding des données

---
+++ models.py
@@ -0,0 +1,14 @@
+from langchain_ollama import OllamaEmbeddings, ChatOllama
+
+class Models:
+    def __init__(self):
+        # ollama pull mxbai-embed-large
+        self.embeddings_ollama = OllamaEmbeddings(
+            model="mxbai-embed-large"
+        )
+
+        # ollama pull llama3.2
+        self.model_ollama = ChatOllama(
+            model="llama3.2",
+            temperature=0,
+        )
