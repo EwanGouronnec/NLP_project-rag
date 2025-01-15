f158e19d59dd29c96643dc96e62b2a7f09ed0401
TENEBAY Océane <127630475+tnb-oceane@users.noreply.github.com>
2025-01-15 08:05:19 +0100

    Update data.py, models.py

--- models.py
+++ models.py
@@ -1,14 +1,32 @@
 from langchain_ollama import OllamaEmbeddings, ChatOllama
 
 class Models:
-    def __init__(self):
-        # ollama pull mxbai-embed-large
-        self.embeddings_ollama = OllamaEmbeddings(
-            model="mxbai-embed-large"
-        )
+    def __init__(self, model="llama", embed="large"):
+        ## Retrieval
+        if embed == "large":
+            # Embedding ollama (mxbai-embed-large)
+            self.embeddings_ollama = OllamaEmbeddings(
+                model="mxbai-embed-large"
+            )
 
-        # ollama pull llama3.2
-        self.model_ollama = ChatOllama(
-            model="llama3.2",
-            temperature=0,
-        )
+        else:
+            # Embedding ollama (all-minilm)
+            self.embeddings_ollama = OllamaEmbeddings(
+                model="all-minilm"
+            )
+
+
+        ## Generation
+        if model == "llama":
+            # Model ollama (llama3.2)
+            self.model_ollama = ChatOllama(
+                model="llama3.2",
+                temperature=0,
+            )
+        
+        else:
+            # Model ollama (mistral)
+            self.model_ollama = ChatOllama(
+                model="mistral",
+                temperature=0,
+            )
